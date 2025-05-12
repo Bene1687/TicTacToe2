d42160621a8e570c12fea595870e74df261db492
Bene1687 <benedicte.challet@gmail.com>
2025-05-12 22:08:04 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using TicTacToe2;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+
+UIMethods.DisplayWelcomeMessage();
+namespace TicTacToe2
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+        
+            //make use of a grid and allow users to place a symbol
+            //think of a way how a user can actually place a symbol into the play area
+            //build an AI that also places their symbol
+            //check when the game is over
+            //keep a clean separation between ui and logic functions
+        }
+    }
+}
---
+++ UIMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe2
+{
+    public static class UIMethods
+    {
+        public static void DisplayWelcomeMessage()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
